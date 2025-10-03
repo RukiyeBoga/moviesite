98494c399c98cc3fdadb1e75c812b8ef16615d90
Rukiye <rukiyeboga4@gmail.com>
2025-10-03 14:36:58 +0300

    moviesite
    web site with tailwind

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MainPage } from './pages/main-page/main-page';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, MainPage],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+})
+export class App {
+  protected readonly title = signal('test');
+}
